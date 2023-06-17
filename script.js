7dd4a8e84b7dd319d22f68ef493d6b12751617e8
Josseline-g <134122936+Josseline-g@users.noreply.github.com>
2023-06-17 08:50:22 -0600

    repositorio

---
+++ script.js
@@ -0,0 +1,15 @@
+var botones = document.getElementById("botones")
+var input = documet.getElementById("input")
+
+function bnt1{
+    if (condition) {
+        
+    } 
+    else {
+        
+    }
+}
+
+function bnt2{
+
+}
